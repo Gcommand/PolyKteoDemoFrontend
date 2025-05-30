9ea60d414946ee76056fd9b11cf0d9872b17208e
Gcommand <gary2000y2k@gmail.com>
2025-05-30 10:07:30 +0800

    patent assignee filter working - fixed the deployment issue and testing on docker

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -18,6 +18,7 @@ export async function GET(request: Request) {
   // let baseUrl = `http://localhost:5000/search?query=${encodeURIComponent(query)}&confidence_level=${confidenceLevel}&sorting_order=${sortingOrder}&current_page=${currentPage}&page_size=${pageSize}`;
   // let baseUrl = `https://poly-kteo-poc-d4c9fkgrbaahe5hg.eastasia-01.azurewebsites.net/search?query=${encodeURIComponent(query)}&confidence_level=${confidenceLevel}&sorting_order=${sortingOrder}&current_page=${currentPage}&page_size=${pageSize}`;
   let baseUrl = `https://gary-testing-avh4dya7dygkddhz.southeastasia-01.azurewebsites.net/search?query=${encodeURIComponent(query)}&confidence_level=${confidenceLevel}&sorting_order=${sortingOrder}&current_page=${currentPage}&page_size=${pageSize}`;
+  // let baseUrl = `https://gt-docker-4-e8cveaecfhhxb9eq.southeastasia-01.azurewebsites.net/search?query=${encodeURIComponent(query)}&confidence_level=${confidenceLevel}&sorting_order=${sortingOrder}&current_page=${currentPage}&page_size=${pageSize}`;
   if (assigneeId) {
     baseUrl += `&assignee_id=${assigneeId}`;
   }
