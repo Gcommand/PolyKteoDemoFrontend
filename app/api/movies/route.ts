25221412858271b6470db8d0c4c6f31dfcbddb98
Gary Chan <gary.chan@catomind.com>
2025-07-06 00:30:39 +0800

    fix for snyk scan - update to latest lib

--- app/api/movies/route.ts
+++ app/api/movies/route.ts
@@ -1,21 +1,20 @@
 import { getSupabaseClient } from "@/lib/supabase/supabase-client";
 import { MovieType } from "@/movies";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-const openAi = new OpenAIApi(configuration);
+const openAi = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
 export async function POST(request: Request) {
   const movie = (await request.json()) as MovieType;
 
   // Create Embedding
-  const embeddingResponse = await openAi.createEmbedding({
+  const embeddingResponse = await openAi.embeddings.create({
     model: "text-embedding-ada-002",
     input: `${movie.title} - ${movie.plot}`,
   });
 
-  const [{ embedding }] = embeddingResponse.data.data;
+  const [{ embedding }] = embeddingResponse.data;
 
   // Insert Into Supabase
   const supabaseClient = getSupabaseClient();
